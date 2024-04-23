b37b71e38cf8d8a30801b99c7712f8ea311f9b50
durga789h <7044q3257@gmail.com>
2024-04-23 11:46:08 +0530

    image change

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import image from '../../public/assets/images/logo.svg'
 
 export default function Footer() {
   return (
     <footer className='border-t'>
       <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
         <Link href={"/"}>
-          <Image src={"/assets/images/logo.svg"} width={120} height={45} alt="logo"/>
+          <Image src={image} width={120} height={45} alt="logo"/>
         </Link>
         <p>2024 Evently. All Rights reserved.</p>
       </div>
